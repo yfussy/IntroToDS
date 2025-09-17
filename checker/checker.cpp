8ba42505366da51f7b14960a3dfb7cd8e9b49ee3
yfussy <toainth@gmail.com>
2025-09-17 12:28:16 +0700

    checker: handles time limit exceed
    - create sub-process to run each test case
    - terminate process if exceed time limit

--- checker/checker.cpp
+++ checker/checker.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <chrono>
 #include <sstream>
+#include <windows.h>
 
 using namespace std;
 
@@ -30,38 +31,45 @@ void answerCheck(ifstream &fout, vector<string> &answer) {
     if (!mismatch) cout << "Answers are correct!" << endl;
 }
 
-template <typename F>
-vector<string> runAndCapture(F func, const string &inputFile) {
-    // Save original buffers
-    streambuf* oldCout = cout.rdbuf();
-    streambuf* oldCin = cin.rdbuf();
+bool runWithTimeout(const string &exe, const string &inFile, const string &tmpFile, int timeoutMs) {
+    // Builds the cmd line for sub process in solver mode
+    string cmd = "\"" + exe + "\" --solve " + inFile + " " + tmpFile;
 
-    // Redirect cout to buffer
-    ostringstream outBuffer;
-    cout.rdbuf(outBuffer.rdbuf());
+    // Info for starting a process
+    STARTUPINFOA si = { sizeof(si) };
+    PROCESS_INFORMATION pi; // handles IDs
 
-    // Redirect cin to file
-    ifstream fin(inputFile);
-    cin.rdbuf(fin.rdbuf());
+    // Starts a process
+    if (!CreateProcessA(
+            NULL,
+            const_cast<char*>(cmd.c_str()), // full cmd line
+            NULL, NULL, FALSE,
+            CREATE_NO_WINDOW, NULL, NULL,
+            &si, &pi)) {
+        cerr << "Failed to start process\n";
+        return false;
+    }
 
-    // Run problem (which now just uses cin/cout)
-    func();
+    // Wait for the process to finish
+    DWORD result = WaitForSingleObject(pi.hProcess, timeoutMs); // timeoutMs: time willing to wait
 
-    // Restore cin and cout
-    cout.rdbuf(oldCout);
-    cin.rdbuf(oldCin);
+    // If time is out, terminate the process
+    if (result == WAIT_TIMEOUT) {
+        TerminateProcess(pi.hProcess, 1);
+        
+        // Clean up
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        return false; // Time limit exceeds
+    }
 
-    // Split captured output
-    vector<string> result;
-    istringstream iss(outBuffer.str());
-    string word;
-    while (iss >> word) result.push_back(word);
-
-    return result;
+    // Clean up
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    return true; // Finished normally
 }
 
-template <typename F>
-void testCases(string name, int caseStart, int caseEnd, F func) {
+void testCases(string name, int caseStart, int caseEnd, const string &exePath) {
     
     for (int i = caseStart; i <= caseEnd; i++) {
         cout << "Test Case: " << i << endl;
@@ -69,27 +77,50 @@ void testCases(string name, int caseStart, int caseEnd, F func) {
         
         string inFile  = "input/"  + dir + "in";
         string outFile = "output/" + dir + "sol";
-
+        string tmpFile = "temp_output.txt";
+        
         ifstream fout(outFile);
         if (!fout.is_open()) {
-            cout << "Test Case not found. Skipping..." << endl;
+            cout << "Test Case not found. Skipping...\n\n";
             continue;
         }
 
         auto start = chrono::high_resolution_clock::now();
-        vector<string> answer = runAndCapture(func, inFile);
+        bool ok = runWithTimeout(exePath, inFile, tmpFile, 1600);
         auto end = chrono::high_resolution_clock::now();
+
+        if (!ok) {
+            cout << "Time limit exceeded. Skipping...\n\n";
+            continue;
+        }
+
+        ifstream ansFile(tmpFile);
+        vector<string> answer;
+        string word;
+        while (ansFile >> word) answer.push_back(word);
         
         answerCheck(fout, answer);
         
         chrono::duration<double> duration = end - start;
-        cout << "Execution time: " << duration.count() << " s" << endl;
-    
-        cout << endl;
+        cout << "Execution time: " << duration.count() << " s\n\n";
     }
 }
 
-int main() {
+// argc: arg counts, argv: arg value
+// checker.exe (--solve inFile outFile)
+// ----[0]----  --[1]-- --[2]- --[3]-- : argv[]
+int main(int argc, char* argv[]) {
     ios_base::sync_with_stdio(false); cin.tie(0);
-    testCases("d62_q1b_queue_count", 1, 1, problem);
+
+    if (argc > 1 && string(argv[1]) == "--solve") {
+        // Solver mode
+        freopen(argv[2], "r", stdin);
+        freopen(argv[3], "w", stdout);
+        problem();
+        return 0;
+    }
+
+    // Checker mode
+    string exePath = argv[0];
+    testCases("d67_q1b_wall_paint", 6, 20, exePath);
 }
